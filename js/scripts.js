166167f0bea792004c3141796490ca2fe8922c79
Kevin VanEvery <kvanever@gmail.com>
2016-06-10 08:27:07 -0700

    add logic to create an array of integers in pingpong

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,18 @@
 /*This file is for your custom js.  All yours*/
+// Business Logic
+
+// Main Ping Pong function
+
+var pingPong = function(integer) {
+  var array = [];
+  debugger;
+  for (i = 1; i <= integer; i++) {
+    array[i] = i;
+  }
+  return array;
+}
+
+// User Interface Logic
 
 // Calls input from form-input.html
 
@@ -10,8 +24,8 @@ $(document).ready(function(){
 
     var output = pingPong(input);
 
-    output.forEach(function(element)) {
-      $('ul#output').append("<li>" + element + ""</li>"");
-    }
+    output.forEach(function(element) {
+      $('ul#output').append("<li>" + element + "</li>");
+    });
   });
 });
